0c152d2562e47ad327a90daf0b17dfc6959dab2b
Konovalov Alexander <konovalov_a.s@mail.ru>
2020-10-01 11:38:39 +0500

    Добавляет валидацию в модальном окне

--- source/js/main.js
+++ source/js/main.js
@@ -4,23 +4,14 @@
   var callOrderButton = document.querySelector('.page-header__order');
   var orderModal = document.querySelector('.modal--order');
 
-  // var modalPhone = modal.querySelector('input[name=modal-phone]');
-  // var modalEmail = modal.querySelector('input[name=modal-email]');
-
   // open modal
   callOrderButton.addEventListener('click', function (evt) {
     evt.preventDefault();
     orderModal.classList.add('modal--show');
+    modalNameInput.focus();
+    modalNameInput.value = localStorage.getItem('modalNameInput');
+    modalPhoneInput.value = localStorage.getItem('modalPhoneInput');
   });
-  // modalOpenButtons.forEach(function (button) {
-  //   button.addEventListener('click', function (evt) {
-  //     evt.preventDefault();
-  //     modal.classList.add('modal--show');
-  //     modalPhone.focus();
-  //     modalPhone.value = localStorage.getItem('modalPhone');
-  //     modalEmail.value = localStorage.getItem('modalEmail');
-  //   });
-  // });
 
   // close modal by close button
   var modalCloseButtons = document.querySelectorAll('.modal__close');
@@ -29,15 +20,6 @@
       orderModal.classList.remove('modal--show');
       successModal.classList.remove('modal--show');
     });
-  });
-
-  // submit order form
-  var orderFrom = document.querySelector('.modal__form');
-  var successModal = document.querySelector('.modal--success');
-  orderFrom.addEventListener('submit', function (evt) {
-    evt.preventDefault();
-    orderModal.classList.remove('modal--show');
-    successModal.classList.add('modal--show');
   });
 
   // close modal success by OK button
@@ -65,6 +47,80 @@
       }
       modal.classList.remove('modal--show');
     });
+  });
+
+  // validate form
+  var orderFrom = document.querySelector('.modal__form');
+  var modalFiled = orderFrom.querySelector('.modal__fileds');
+  var modalNameInput = orderFrom.querySelector('#modal-name');
+  var modalPhoneInput = orderFrom.querySelector('#modal-phone');
+
+  var addInvalidClass = function () {
+    modalFiled.classList.remove('modal__fileds--valid');
+    modalFiled.classList.add('modal__fileds--invalid');
+  };
+
+  var addValidClass = function () {
+    modalFiled.classList.add('modal__fileds--valid');
+    modalFiled.classList.remove('modal__fileds--invalid');
+  };
+
+  var validateName = function () {
+    if (modalNameInput.validity.valueMissing) {
+      modalNameInput.setCustomValidity('Как тебя зовут?');
+      addInvalidClass();
+      return;
+    }
+    if (modalNameInput.validity.tooShort) {
+      modalNameInput.setCustomValidity('Не меньше ' + modalNameInput.minLength + ' символов');
+      addInvalidClass();
+      return;
+    }
+    modalNameInput.setCustomValidity('');
+    addValidClass();
+    localStorage.setItem('modalNameInput', modalNameInput.value);
+  };
+
+  var validatePhone = function () {
+    if (modalPhoneInput.validity.valueMissing) {
+      modalPhoneInput.setCustomValidity('Укажи номер телефона');
+      addInvalidClass();
+      return;
+    }
+    if (modalPhoneInput.validity.patternMismatch) {
+      modalPhoneInput.setCustomValidity('Укажи 10 цифр номера телефона без 8 или +7');
+      addInvalidClass();
+      return;
+    }
+    modalPhoneInput.setCustomValidity('');
+    addValidClass();
+    localStorage.setItem('modalPhoneInput', modalPhoneInput.value);
+  };
+
+  modalNameInput.addEventListener('invalid', function () {
+    validateName();
+  });
+
+  modalNameInput.addEventListener('input', function () {
+    validateName();
+  });
+
+  modalPhoneInput.addEventListener('invalid', function () {
+    validatePhone();
+  });
+
+  modalPhoneInput.addEventListener('input', function () {
+    validatePhone();
+  });
+
+  // submit order form
+  var successModal = document.querySelector('.modal--success');
+  orderFrom.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    validateName();
+    validatePhone();
+    orderModal.classList.remove('modal--show');
+    successModal.classList.add('modal--show');
   });
 
 })();
