c3397d870bdbf42f20a2bfe3a7408aca23541eee
Konovalov Alexander <konovalov_a.s@mail.ru>
2020-10-02 11:23:50 +0500

    Добавляет блок программы

--- source/js/main.js
+++ source/js/main.js
@@ -123,4 +123,46 @@
     successModal.classList.add('modal--show');
   });
 
+  // switch tabs - programs
+  function hideCurrentTab() {
+    document.querySelector('.js-tab--show').classList.remove('js-tab--show');
+    document.querySelector('.js-tab--active').classList.remove('js-tab--active');
+  }
+
+  function showTab(evt) {
+    hideCurrentTab();
+    var evtButton = evt.target;
+    evtButton.classList.add('js-tab--active');
+    var buttonClass = evt.target.classList[1];
+    var content = document.querySelectorAll('.' + buttonClass)[1];
+    content.classList.add('js-tab--show');
+  }
+
+  function hideTabs() {
+    var tabs = document.querySelectorAll('.js-tab--content');
+    tabs.forEach(function (tab) {
+      tab.classList.add('js-tab--hide');
+    });
+  }
+
+  var tabButton = document.querySelectorAll('button.js-tab');
+  tabButton.forEach(function (button) {
+    button.addEventListener('click', showTab);
+  });
+
+  window.addEventListener('load', hideTabs);
+
+  // go to tab
+  var cards = document.querySelectorAll('.places-visit__link');
+  cards.forEach(function (card) {
+    card.addEventListener('click', function (evt) {
+      hideCurrentTab();
+      var tabTargetId = evt.target.closest('.places-visit__link').attributes.href.value.slice(1);
+      var targetButton = document.querySelector('.' + tabTargetId);
+      targetButton.classList.add('js-tab--active');
+      var tatTarget = document.querySelector('#' + tabTargetId);
+      tatTarget.classList.add('js-tab--show');
+    });
+  });
+
 })();
